1685e0ac40c826c5544e45d2afe4ea88278f362e
Cayla Bradley <caylabradleydev@gmail.com>
2024-04-30 11:52:55 -0500

    implement search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,14 +1,14 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -25,7 +25,23 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    //Get Search Query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //load results
+    await model.loadSearchResults(query);
+
+    //render results
+    resultsView.render(model.state.search.results);
+  } catch (err) {}
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
